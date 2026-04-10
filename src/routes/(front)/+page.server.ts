60b10c7bf23e16c9a423f8341d7b63b91b000543
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2026-04-10 15:55:56 -0400

    generate migration

--- src/routes/(front)/+page.server.ts
+++ src/routes/(front)/+page.server.ts
@@ -1,4 +1,4 @@
 import { getDb } from '$lib/server/db';
-import type { PageServerLoad } from '../$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (params) => {};
